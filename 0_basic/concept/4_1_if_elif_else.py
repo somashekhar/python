4910317cfac8ba09bc451ff0b2f51223cce46ffa
Som <somashekharvb@gmail.com>
2018-10-16 14:38:57 +0530

    if_elif_else conditional

---
+++ 0_basic/concept/4_1_if_elif_else.py
@@ -0,0 +1,12 @@
+'''
+if..elif..else conditional 
+'''
+
+condition_var = 5
+
+if condition_var > 10 :
+    print("Inside the if loop suite")
+elif condition_var > 5:
+    print("Inside the elif loop suite")
+else:
+    print("Inside the else loop suite")

5507670c0d660348f9fb8a3bdeaeaad9c5a1b988
Som <somashekharvb@gmail.com>
2018-10-16 14:17:07 +0530

    variables

---
+++ 0_basic/concept/1_variable.py
@@ -0,0 +1,13 @@
+'''
+variables
+variables_name = value
+'''
+from builtins import str
+
+a = b = c = 1
+
+x, y, z = 1, 2.9, "Som"
+
+print("The value of a: " + str(a) + " b: "+ str(b) + " c: "+ str(c) +"\n")
+
+print("The value of x: " + str(x) + " y: "+ str(y) + " z: "+ str(z) +"\n")

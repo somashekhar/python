3f9c56a18e1f7300728c1205e227340318f7e329
Som <somashekharvb@gmail.com>
2020-03-18 18:59:08 +0530

    Create 4_2_for_loop.py

---
+++ 0_basic/concept/4_2_for_loop.py
@@ -0,0 +1,18 @@
+"""
+There are 2 types of for loop
+
+for <variable> in <sequence>:
+    #block of statements
+    
+for <variable> in range(start, end, step):
+    #block of statements
+
+"""
+
+foo_bar = ('Apple', 'Banana', 'Mango')
+
+for number in foo_bar:
+    print(number)
+     
+for index in range(0, len(foo_bar))
+    print(foo_bar[index])

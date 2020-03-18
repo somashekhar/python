1fd61b4b57b0bfd5d3b81816edf80b54aa07b579
Som <somashekharvb@gmail.com>
2020-03-18 18:31:25 +0530

    Update 2_datatype.py

--- 0_basic/concept/2_datatype.py
+++ 0_basic/concept/2_datatype.py
@@ -3,11 +3,12 @@ Python has the following data types built-in by default, in these categories:
 
 Text Type     :	str
 Numeric Types :	int, float, complex
+Boolean Type  :	bool
+Binary Types  :	bytes, bytearray, memoryview
+
 Sequence Types:	list, tuple, range
 Mapping Type  :	dict
 Set Types     :	set, frozenset
-Boolean Type  :	bool
-Binary Types  :	bytes, bytearray, memoryview
 
 get the data type of any object by using the type() function
 '''
@@ -18,14 +19,15 @@ x = str("Hello World")	                      str
 x = int(20)	                                  int	
 x = float(20.5)	                              float	
 x = complex(1j)	                              complex	
+x = bool(5)	                                  bool	
+x = bytes(5)	                                bytes	
+
 x = list(("apple", "banana", "cherry"))	      list	
 x = tuple(("apple", "banana", "cherry"))	    tuple	
 x = range(6)	                                range	
 x = dict(name="John", age=36)	                dict	
 x = set(("apple", "banana", "cherry"))	      set	
 x = frozenset(("apple", "banana", "cherry"))	frozenset	
-x = bool(5)	                                  bool	
-x = bytes(5)	                                bytes	
 x = bytearray(5)	                            bytearray	
 x = memoryview(bytes(5))	                    memoryview
 

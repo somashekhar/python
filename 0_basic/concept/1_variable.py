4ffa3d2bf7b846f7ccf552ce72a71e5d6719fbbc
Som <somashekharvb@gmail.com>
2020-03-18 18:08:56 +0530

    Update 1_variable.py

--- 0_basic/concept/1_variable.py
+++ 0_basic/concept/1_variable.py
@@ -11,3 +11,19 @@ x, y, z = 1, 2.9, "Som"
 print("The value of a: " + str(a) + " b: "+ str(b) + " c: "+ str(c) +"\n")
 
 print("The value of x: " + str(x) + " y: "+ str(y) + " z: "+ str(z) +"\n")
+
+
+
+'''
+When you want to use a global variable declare it before functions and then use a global key to access it inside the functions
+'''
+name = 'Som'
+
+def update_name():
+  global name
+  name = "Soma"
+
+print(name)
+update_name()
+print(name)
+  

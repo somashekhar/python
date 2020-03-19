d99b6bac8beeaac15f8492531a5db0a8fdd52d04
Som <somashekharvb@gmail.com>
2020-03-19 10:59:40 +0530

    Create strings.py

---
+++ 1_com_dtypes/concepts/strings.py
@@ -0,0 +1,12 @@
+'''
+String literal are surrounded by single, double or tripple(can use single or double quotes and used for multiline string).
+    'Som'    "Som"    '''Som'''    """Som"""
+
+'''
+
+
+#Python does not have a character data type, a single character is simply a string with a length of 1.
+#Strings are like array and we can access using position
+
+
+
